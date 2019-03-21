2e5c716476f5012bb308157fa26bbc3c7e4f694c
Zhuxm <zhuxiaoming@fiorentini.com.cn>
2019-03-21 14:52:03 +0800

    新增服务

---
+++ src/Student.Application/Imp/UserService.cs
@@ -0,0 +1,15 @@
+﻿using Student.Application.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student.Application.Imp
+{
+    public class UserService : IUserService
+    {
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Student.Application/Interface/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student.Application.Interface
+{
+    public interface IUserService
+    {
+        void Add();
+
+    }
+}
